d644ed5c1d8948362b566b8a6f79a81f816bc68c
ShevaVarya <shevchik.varvara@mail.ru>
2024-04-01 14:37:43 +0200

    Sprint 8. 1 and 2 homeworks

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.button_search)
+        val mediaButton = findViewById<Button>(R.id.button_media)
+        val settingsButton = findViewById<Button>(R.id.button_settings)
+
+        val searchButtonClickListener: View.OnClickListener = object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                printToast("Экран поиска...")
+            }
+        }
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        mediaButton.setOnClickListener {  printToast("Экран медиатеки...") }
+
+        settingsButton.setOnClickListener {  printToast("Экран настроек...") }
     }
+
+    fun printToast (message: String) {
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+    }
+
+
 }
